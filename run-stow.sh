3928d252982d41912102a735267b22e5c607c428
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-11-25 12:29:01 +1030

    script: run master script to connect all configuration

---
+++ run-stow.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+stow -t ~ 1password
+stow -t ~ bash
+stow -t ~ fcitx5
+stow -t ~ ghostty
+stow -t ~ git
+stow -t ~ hyprland
+stow -t ~ nvim
+stow -t ~ starship
+stow -t ~ tmux
+stow -t ~ walker
+
+sudo stow -t /etc keyd
