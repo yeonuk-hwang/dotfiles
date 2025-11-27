6b21eb1b4b63e8fe1da8af9fb3d64e474bcd0bfa
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-11-27 18:10:33 +1030

    nvim: disable lazyload of obsidian plugin as it is not worked well

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -2,14 +2,6 @@ return {
   {
     "obsidian-nvim/obsidian.nvim",
     version = "*",
-    event = {
-      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-      -- refer to `:h file-pattern` for more examples
-      "BufReadPre ~/Dropbox/Obsidian/personal/",
-      "BufEnter oil:///~/Dropbox/Obsidian/personal/",
-      "BufNewFile ~/Dropbox/Obsidian/personal/*.md",
-    },
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
